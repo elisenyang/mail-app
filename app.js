c9f915d95d0bba61356c2ae2f84a4fdbe5ed172e
elisenyang <ely@DESKTOP-0BJ42OG.redmond.corp.microsoft.com>
2018-08-17 16:19:08 -0700

    auth completed

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const passport = require('passport');
+const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
+const config = require('./utils/config.js');
 const MicrosoftGraph = require("@microsoft/microsoft-graph-client");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,17 +21,101 @@ app.engine('hbs', hbs);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//authentication
+const authCallback = (iss, sub, profile, accessToken, refreshToken, done) => {
+    if (!profille.oid) {
+        return done(new Error("No oid found"), null)
+    }
+
+    findByOid(profile.oid, function(err, user){
+        if (err) {
+            return done(err)
+        }
+        if (!user) {
+            users.push({profile, accessToken, refreshToken});
+            return done(null, profile)
+        }
+        return done(null, user)
+    })
+}
+
+passport.use(new OIDCStrategy(config.creds, authCallback));
+
+const users = [];
+
+passport.serializeUser((user, done) => {
+    done(null, user.oid);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    findByOid(id, function (err, user) {
+      done(err, user);
+    });
+  });
+  
+  var findByOid = function(oid, fn) {
+    for (var i = 0, len = users.length; i < len; i++) {
+      var user = users[i];
+      if (user.profile.oid === oid) {
+        return fn(null, user);
+      }
+    }
+    return fn(null, null);
+  };
+
+
+//configurations
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ 'extended': 'false'}));
 app.use(cookieParser());
+app.use(session({
+    secret: 'sshhhhhh',
+    name: 'graphNodeCookie',
+    resave: false,
+    saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 //routes
 app.get('/', function(req, res) {
-    res.render('home')
+    if (req.user) {
+        //var client = Microsoft
+        console.log("user", req.user)
+        res.render('home', {
+            authenticated: true
+        })
+    } else {
+        res.render('home', {
+            authenticated: false
+        })
+    }
 })
 
-app.get('/token', function(req, res) {
-    res.render('token')
+app.post('/', function(req, res) {
+    console.log("USER", req.user)
+    res.render('home', {
+        authenticated: true
+    })
+})
+
+
+app.get('/login',
+  function(req, res, next) {
+    passport.authenticate('azuread-openidconnect',
+    {
+      response: res,
+      failureRedirect: '/'
+    })(req, res, next);
+  },
+  function (req, res) {
+    res.redirect('/');
+});
+
+app.get('/logout', function(req,res){
+    req.logOut(
+    res.redirect('/')
+    )
 })
 
 app.listen(port);
--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     creds: {
         identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
-        clientId: '25ac6d4b-c5ca-476a-867f-bab0b52e3405', //MOVE THIS LATER
+        clientID: '25ac6d4b-c5ca-476a-867f-bab0b52e3405', //MOVE THIS LATER
         responseType: 'code',
         responseMode: 'form_post',
-        redirectURL: 'http://localhost:3000/token',
+        redirectUrl: 'http://localhost:3000/',
         passReqToCallback: true,
         allowHttpForRedirectUrl: true,
         clientSecret: 'xpBR745#}+ydrzwOGCXF77{', //MOVE THIS LATER
